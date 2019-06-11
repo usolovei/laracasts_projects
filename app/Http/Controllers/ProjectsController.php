9b776b35063fe16ee0816fb722c7fbb89b63b6b2
NINT3ND0 <usalawei@gmail.com>
2019-06-11 07:49:54 +0300

    Initial commit

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        return view('projects.index', compact('projects'));
+    }
+
+
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+
+    public function show(Project $project)
+    {
+        return view('projects.show', compact('project'));
+    }
+
+
+    public function edit(Project $project)
+    {
+        return view('projects.edit', compact('project'));
+    }
+
+
+    public function update(Project $project)
+    {
+         $project->update(request(['title', 'description']));
+         return redirect('/projects');
+    }
+
+
+    public function destroy(Project $project)
+    {
+        $project->delete();
+        return redirect('/projects');
+    }
+
+
+    public function store()
+    {
+        Project::create(request(['title', 'description']));
+        return redirect('/projects');
+    }
+}
